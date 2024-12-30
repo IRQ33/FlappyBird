f8b7b1dfa22b81752567c6cfcbb7d34cf6480a53
IRQ3 <gnardoor@gmail.com>
2024-12-30 17:16:58 +0100

    update 30.12.2024

--- core/src/main/java/com/irq3/multiplayer/Managers/PipeManager.java
+++ core/src/main/java/com/irq3/multiplayer/Managers/PipeManager.java
@@ -30,7 +30,7 @@ public class PipeManager {
             pipe.getRectangle().x = (float) (pipe.getElementX() - FastConfig.pipeSpeed);
             if (pipe.getElementX() <= -300) {
                 MainScreen.hierarchyManager.deleteElement(pipe);
-                iterator.remove(); // fuck instagram
+                iterator.remove();
             }
         }
     }
